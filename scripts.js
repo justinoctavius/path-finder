c22a0566567ace9c99fe14f49e220c4718fcd637
octavio <justinoctavio2001@gmail.com>
2020-06-02 15:55:47 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,197 @@
+function removeFromArray(arr, elt){
+    for(var i = arr.length-1; i>=0; i--){
+        if(arr[i] == elt){
+            arr.splice(i, 1);
+        } 
+    }
+}
+
+function heuristic(a,b){
+    // var d = dist(a.i,a.j,b.i,b.j);
+    var d = abs(a.i - b.i) + abs(a.j - b.j);
+    return d;
+}
+
+var cols = 40;
+var rows = 40;
+var grid = new Array(cols);
+
+var openSet = [];
+var closedSet = [];
+var start;
+var end;
+var w,h;
+var path = [];
+var noSolution = false;
+
+
+function Spot(i,j){
+    this.i = i;
+    this.j = j;
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+    this.neighbors = [];
+    this.previous = undefined;
+    this.wall = false;
+
+    if(random(1) < 0.4){
+        this.wall = true;
+    }
+
+    this.show = function(col){
+        fill(col);
+        if(this.wall){
+            fill(0)
+        }
+        strokeWeight(1)
+        stroke(0);
+        rect( this.i*w, this.j*h, w-1, h-1);
+    }
+
+    this.addNeighbors = function(grid){
+        var i = this.i;
+        var j = this.j;
+        if(i < cols-1){
+            this.neighbors.push(grid[i + 1][j]);
+        }
+        if(i > 0){
+            this.neighbors.push(grid[i - 1][j]);
+        }
+        if(j < rows - 1 ){
+            this.neighbors.push(grid[i][j + 1]);
+        }
+        if(j > 0){
+            this.neighbors.push(grid[i][j - 1]);
+        }
+        if(j > 0 && i > 0){
+            this.neighbors.push(grid[i - 1][j - 1]);
+        }
+        if(j < rows - 1 && i < cols - 1){
+            this.neighbors.push(grid[i + 1][j + 1]);
+        }
+
+
+    }
+
+}
+
+
+function setup(){
+    createCanvas(400,400);
+    w = width/cols;
+    h = height/rows;
+
+    //making a 2D array
+    for (let i = 0; i < cols; i++){
+        grid[i] = new Array(rows);
+    }
+
+    for(let i = 0;i < cols; i++){
+        for(let j = 0; j < rows; j++){
+            grid[i][j] = new Spot(i,j);
+        }
+    }
+    
+    for(let i = 0;i < cols; i++){
+        for(let j = 0; j < rows; j++){
+            grid[i][j].addNeighbors(grid)
+        }
+    }
+
+    start = grid[0][0];
+    end = grid[cols -1][rows/2];
+    start.wall = false;
+    end.wall = false;
+
+    openSet.push(start);
+
+    console.log(grid)
+
+}
+
+function draw(){
+    background(0);
+
+    if(openSet.length > 0){
+        //we can keep going
+        var winner = 0;
+        for (var i = 0; i < openSet.length; i++){
+            if(openSet[i].f < openSet[winner].f){
+                winner = i;
+            }
+        }
+
+        var current = openSet[winner];
+
+        if(openSet[winner] === end){
+
+            
+            noLoop();
+            console.log('DONE!');
+        }
+
+        removeFromArray(openSet, current)
+        closedSet.push(current);
+
+        var neighbors = current.neighbors;
+        for(var i = 0; i < neighbors.length; i++ ){
+            var neighbor = neighbors[i];
+            
+            if(!closedSet.includes(neighbor) && !neighbor.wall){
+                var tempG = current.g + 1;
+            
+                if(openSet.includes(neighbor)){
+                    if(tempG < neighbor.g){
+                        neighbor.g = tempG;
+                    }
+                }else{
+                    neighbor.g = tempG;
+                    openSet.push(neighbor);
+                }
+            
+                neighbor.h = heuristic(neighbor,end);
+                neighbor.f = neighbor.g + neighbor.h;
+                neighbor.previous = current;
+            }
+            
+        }
+
+    } else {
+        //no solution
+        console.log("no solution");
+        noSolution = true
+        noLoop();
+    }
+
+    for(let i = 0; i < cols; i++){
+        for(let j = 0; j < rows; j++){
+            grid[i][j].show(color(255));
+            end.show(color(255,0,250))
+        }
+    }
+
+    for (var i = 0; i < closedSet.length; i++){
+        closedSet[i].show(color(255,0,0));
+    }
+    
+    for (var i = 0; i < openSet.length; i++){
+        openSet[i].show(color(0,255,0));
+    }
+
+    if(!noSolution){
+        //find the path
+        path = [];
+        var temp = current;
+        path.push(temp);
+        while(temp.previous){
+            path.push(temp.previous);
+            temp = temp.previous;
+        }
+    }
+
+    for (var i = 0; i < path.length; i++){
+        path[i].show(color(0,0,255));
+    }
+
+}
